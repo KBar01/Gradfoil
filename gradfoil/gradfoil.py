678c426fb4df9228a4c6e4b5cac81ab31b7f9e99
KBar01 <pa20830@bristol.ac.uk>
2025-05-29 09:22:57 +0100

    extra flag in input

--- gradfoil/gradfoil.py
+++ gradfoil/gradfoil.py
@@ -23,7 +23,8 @@ def standard_run(alphaDeg,Re,Ma,xcoords,ycoords):
         "Re": Re,
         "Ma": Ma,
         "restart": 0,
-        "xfoilStart":0
+        "xfoilStart":0,
+        "xfoilgetpoints":0
     }
 
     # Write JSON input
