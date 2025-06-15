5d9331199bf05a043e6343332d742413d41c7464
KBar01 <pa20830@bristol.ac.uk>
2025-06-15 12:21:35 +0100

    adaptive smapling approach at TE

--- src/extract_BL_TE.cpp
+++ src/extract_BL_TE.cpp
@@ -8,7 +8,7 @@
 
 
 // Performs cubic interpolation at x using 4 surrounding points (xs, ys)
-Real cubic_interp(Real x, const Real xs[4], const Real ys[4]) {
+Real cubic_interp(Real x, const Real* xs, const Real* ys) {
     Real result = 0.0;
     for (int i = 0; i < 4; ++i) {
         Real term = ys[i];
@@ -21,13 +21,43 @@ Real cubic_interp(Real x, const Real xs[4], const Real ys[4]) {
     return result;
 }
 
+Real linear_interp(Real x, const Real* xs, const Real* ys) {
+    return ys[0] + (ys[1] - ys[0]) * (x - xs[0]) / (xs[1] - xs[0]);
+}
+
+Real quadratic_interp(Real x, const Real* xs, const Real* ys) {
+    Real result = 0.0;
+    for (int i = 0; i < 3; ++i) {
+        Real term = ys[i];
+        for (int j = 0; j < 3; ++j) {
+            if (i != j)
+                term *= (x - xs[j]) / (xs[i] - xs[j]);
+        }
+        result += term;
+    }
+    return result;
+}
+
+Real adaptive_interp(Real x, const Real* xs, const Real* ys, int n) {
+    if (n == 4)
+        return cubic_interp(x, xs, ys);
+    else if (n == 3)
+        return quadratic_interp(x, xs, ys);
+    else if (n == 2)
+        return linear_interp(x, xs, ys);
+    else
+        return 0.0; // fallback error
+}
+
 int find_interp_position(const Real* xcoords, int start, int end, Real x_target) {
 
-    // Prepare 4-point stencil for x around x_target (shared between both interpolations)
+    // For top surface : finds node just before sampling pos
+    // For bot surface : finds node just before sampling pos
+    
     int found_idx = -1;
     bool is_increasing = xcoords[start] < xcoords[start + 1];
 
-    for (int i = start + 1; i < end - 2; ++i) {
+    for (int i = start; i < end - 1; ++i) {
         if (is_increasing) {
             if (xcoords[i] <= x_target && x_target < xcoords[i + 1]) {
                 found_idx = i;
@@ -35,7 +65,7 @@ int find_interp_position(const Real* xcoords, int start, int end, Real x_target)
             }
         } else {
             if (xcoords[i] >= x_target && x_target > xcoords[i + 1]) {
-                found_idx = i;
+                found_idx = i+1;
                 break;
             }
         }
@@ -44,147 +74,110 @@ int find_interp_position(const Real* xcoords, int start, int end, Real x_target)
     return found_idx;
 }
 
+void get_nodes(int topFoundIdx,int botFoundIdx, Real x_target, int* topNodeList,int topNnodes, int* botNodeList,int botNnodes, const Vsol&vsol){
+
+    // do top surface :
+    int topStart = topFoundIdx - 1 ; // Ideal starting position for cubic interp to have 2 nodes either side of sampling position
+    topNnodes = 4 ;
+    if (vsol.turb[topFoundIdx] == false){
+        topNnodes = 0;
+    }
+    else{
+        if (vsol.turb[topStart] == false){topStart += 1; } // original start is not turbulent, shift start down a node
+        topNnodes = (Ncoords-1 - topStart) + 1 ;  // how many available nodes to use for interp
+        if (topNnodes>4){topNnodes=4;} // limit to 4 nodes for cubic
+    }
+
+    for (int i=0;i<topNnodes;++i){topNodeList[i] = topStart+i ;}
 
 
-/*
- * Interpolates the 4 flow quantities at x_target on one surface (top or bottom).
- * Inputs:
- *   - xcoords: array of x-coordinates [size N]
- *   - states: flattened 1D array of 4*N flow states (4 values per node)
- *   - start, end: range of indices defining the surface segment (e.g., 0 to N/2)
- *   - x_target: x-position at which to interpolate (e.g., 0.95 for 95% chord)
- * Outputs:
- *   - interpolated_states: array of 4 values, filled with interpolated results
- */
-void interpolate_on_surface(const Real* xcoords, const Real* states, int found_idx, Real x_target, Real* interpolated_states, const Vsol&vsol) {
-   
-    Real xs[4] = {
-        xcoords[found_idx - 1],
-        xcoords[found_idx],
-        xcoords[found_idx + 1],
-        xcoords[found_idx + 2]
+    // do bot surface :
+    int botStart = botFoundIdx + 1 ; // Ideal starting position for cubic interp to have 2 nodes either side of sampling position
+    botNnodes = 4;
+    if (vsol.turb[botFoundIdx] == false){
+        botNnodes = 0;
+    }
+    else{
+        if (vsol.turb[botStart] == false){botStart -= 1; } // original start is not turbulent, shift start down a node
+        int botNnodes = botStart ;  // how many available nodes to use for interp
+        if (botNnodes>4){botNnodes=4;} // limit to 4 nodes for cubic
+    }
+
+    for (int i=0;i<botNnodes;++i){botNodeList[i] = botStart-i ;}
+}
+
+
+
+void interp_BL_states(const int* topIdx,const int* botIdx, const int topNnodes, const int botNnodes, const Real x_target, const Real* xcoords, const Real* states, Real* topInterpStates, Real* botInterpStates){
+
+
+
+    // Do top surface first 
+    Real txs[4] = {
+        xcoords[topIdx[0]],
+        xcoords[topIdx[1]],
+        xcoords[topIdx[2]],
+        xcoords[topIdx[3]]
     };
 
-    int entries[3] = {0,1,3};
-    for (int q = 0; q < 3; ++q) {
-        Real ys[4] = {
-            states[colMajorIndex(entries[q],found_idx-1,4)],
-            states[colMajorIndex(entries[q],found_idx  ,4)],
-            states[colMajorIndex(entries[q],found_idx+1,4)],
-            states[colMajorIndex(entries[q],found_idx+2,4)]
+    for (int q = 0; q < 4; ++q) {
+        Real tys[4] = {
+            states[colMajorIndex(q,topIdx[0],4)],
+            states[colMajorIndex(q,topIdx[1],4)],
+            states[colMajorIndex(q,topIdx[2],4)],
+            states[colMajorIndex(q,topIdx[3],4)]
         };
-        interpolated_states[entries[q]] = cubic_interp(x_target, xs, ys);
+        topInterpStates[q] = adaptive_interp(x_target,txs,tys,topNnodes);
     }
 
-    // Checking Ctau interp for no transition or laminar node inclusion
-    bool bottom = true;
-    if (found_idx > 100){bottom = false;}
+    // then bottom surface
 
-    bool laminarBL = false; 
-    Real ctauX[4], cTau[4] ;
+    Real bxs[4] = {
+        xcoords[botIdx[0]],
+        xcoords[botIdx[1]],
+        xcoords[botIdx[2]],
+        xcoords[botIdx[3]]
+    };
 
-    if (bottom){
-
-        if (vsol.turb[found_idx+1] == false){
-
-            interpolated_states[2] = 0; // hasnt transitione before 95%, no turb BL value
-            laminarBL = true ;
-        }
-        else if (vsol.turb[found_idx+2] == false){
-            // the last entry on interp grid would be laminar, step grid back to make sure all turb 
-            
-            ctauX[0] = xcoords[found_idx -2];
-            ctauX[1] = xcoords[found_idx -1];
-            ctauX[2] = xcoords[found_idx   ];
-            ctauX[3] = xcoords[found_idx +1];
-
-            cTau[0] = states[colMajorIndex(2,found_idx-2,4)];
-            cTau[1] = states[colMajorIndex(2,found_idx-1,4)];
-            cTau[2] = states[colMajorIndex(2,found_idx  ,4)];
-            cTau[3] = states[colMajorIndex(2,found_idx+1,4)];
-        }
-        else{
-            
-            // use the normal grid
-            ctauX[0] = xcoords[found_idx -1];
-            ctauX[1] = xcoords[found_idx   ];
-            ctauX[2] = xcoords[found_idx +1];
-            ctauX[3] = xcoords[found_idx +2];
-
-            cTau[0] = states[colMajorIndex(2,found_idx-1,4)];
-            cTau[1] = states[colMajorIndex(2,found_idx  ,4)];
-            cTau[2] = states[colMajorIndex(2,found_idx+1,4)];
-            cTau[3] = states[colMajorIndex(2,found_idx+2,4)];
-        }
-    }
-    else {
-
-        if (vsol.turb[found_idx] == false){
-            
-            interpolated_states[2] = 0; // hasnt transitione before 95%, no turb BL value
-            laminarBL = true ;
-        }
-        else if (vsol.turb[found_idx-1] == false){
-            
-            // first node in grid was laminar, step forward to make all turb
-            ctauX[0] = xcoords[found_idx   ];
-            ctauX[1] = xcoords[found_idx +1];
-            ctauX[2] = xcoords[found_idx +2];
-            ctauX[3] = xcoords[found_idx +3];
-
-            cTau[0] = states[colMajorIndex(2,found_idx  ,4)];
-            cTau[1] = states[colMajorIndex(2,found_idx+1,4)];
-            cTau[2] = states[colMajorIndex(2,found_idx+2,4)];
-            cTau[3] = states[colMajorIndex(2,found_idx+3,4)];
-
-        }
-        else{
-            // use the normal grid
-            ctauX[0] = xcoords[found_idx -1];
-            ctauX[1] = xcoords[found_idx   ];
-            ctauX[2] = xcoords[found_idx +1];
-            ctauX[3] = xcoords[found_idx +2];
-
-            cTau[0] = states[colMajorIndex(2,found_idx-1,4)];
-            cTau[1] = states[colMajorIndex(2,found_idx  ,4)];
-            cTau[2] = states[colMajorIndex(2,found_idx+1,4)];
-            cTau[3] = states[colMajorIndex(2,found_idx+2,4)];
-        }
-    }
-
-    if (!laminarBL){
-        interpolated_states[2] = cubic_interp(x_target, ctauX, cTau);
+    for (int q = 0; q < 4; ++q) {
+        Real bys[4] = {
+            states[colMajorIndex(q,botIdx[0],4)],
+            states[colMajorIndex(q,botIdx[1],4)],
+            states[colMajorIndex(q,botIdx[2],4)],
+            states[colMajorIndex(q,botIdx[3],4)]
+        };
+        botInterpStates[q] = adaptive_interp(x_target,bxs,bys,topNnodes);
     }
 }
 
 
 // Computes dp/dx at x_target using cubic interpolation + central finite difference
-Real interpolate_dpdx(const Real* xcoords, const Real* Cps, int found_idx, Real x_target, const Oper&oper,const Real Uinf) {
+Real interpolate_dpdx(const Real* xcoords, const Real* Cps, const int* nodeIdx, const int nodeN, Real x_target, const Oper&oper,const Geom&geom,const Real Uinf) {
     
     
-    Real h = 1e-6;  // Small step size for derivative approximation TODO: verfiy step is correct (convergence)
+    Real h = 1e-6 * geom.chord ;  // Small step size for derivative approximation TODO: verfiy step is correct (convergence)
 
     // Get x positions slightly left and right of target
     Real x_plus = x_target + h;
     Real x_minus = x_target - h;
 
     Real xs[4] = {
-        xcoords[found_idx - 1],
-        xcoords[found_idx],
-        xcoords[found_idx + 1],
-        xcoords[found_idx + 2]
+        xcoords[nodeIdx[0]],
+        xcoords[nodeIdx[1]],
+        xcoords[nodeIdx[2]],
+        xcoords[nodeIdx[3]]
     };
 
     Real ps[4] = {
-        Cps[found_idx - 1],
-        Cps[found_idx],
-        Cps[found_idx + 1],
-        Cps[found_idx + 2]
+        Cps[nodeIdx[0]],
+        Cps[nodeIdx[1]],
+        Cps[nodeIdx[2]],
+        Cps[nodeIdx[3]]
     };
 
     // Interpolate pressure at x+h and x-h
-    Real CpPlus  = cubic_interp(x_plus, xs, ps);
-    Real CpMinus = cubic_interp(x_minus, xs, ps);
+    Real CpPlus  = adaptive_interp(x_plus,xs,ps,nodeN);
+    Real CpMinus = adaptive_interp(x_minus,xs,ps,nodeN);
 
     // Central finite difference
     Real dpdx = ((CpPlus - CpMinus) / (2.0 * h)) * (0.5 * oper.rho * Uinf*Uinf);
@@ -193,19 +186,19 @@ Real interpolate_dpdx(const Real* xcoords, const Real* Cps, int found_idx, Real 
 }
 
 
-Real interpolate_cf(const Real* xcoords, const Real* states, const Vsol&vsol, int found_idx, Real x_target, const Param&param) {
+Real interpolate_cf(const Real* xcoords, const Real* states, const int* nodeIdx, const int nodeN, Real x_target, const Vsol&vsol, const Param&param) {
     
     Real xs[4] = {
-        xcoords[found_idx - 1],
-        xcoords[found_idx],
-        xcoords[found_idx + 1],
-        xcoords[found_idx + 2]
+        xcoords[nodeIdx[0]],
+        xcoords[nodeIdx[1]],
+        xcoords[nodeIdx[2]],
+        xcoords[nodeIdx[3]]
     };
 
 
     Real cf[4];
 
-    int indexes[4] = {found_idx-1, found_idx, found_idx+1, found_idx +2} ;
+    int indexes[4] = {nodeIdx[0], nodeIdx[1],nodeIdx[2], nodeIdx[3]} ;
     Real cf_U[4] = {0};
     for (int i=0;i<4;++i){
         cf[i] = get_cf(
@@ -220,23 +213,12 @@ Real interpolate_cf(const Real* xcoords, const Real* states, const Vsol&vsol, in
         );
     }
     // Interpolate 
-    Real Cf95  = cubic_interp(x_target, xs, cf);
+    Real Cf95  = adaptive_interp(x_target,xs,cf,nodeN);
 
     return Cf95;
 }
 
 
-
-/*
- * Master function to interpolate 4 flow quantities at 95% chord on both surfaces.
- * Inputs:
- *   - xcoords: array of x-coordinates, size N
- *   - states: flattened array of 4*N states, with 4 values per node
- *   - N: total number of surface nodes (must be even, with bottom and top halves)
- * Outputs:
- *   - interp_bottom: interpolated 4 values on bottom surface at 95% chord
- *   - interp_top: interpolated 4 values on top surface at 95% chord
- */
 void interpolate_at_95_both_surfaces(const Real* xcoords, const Real* states, const Real*Cps, const Oper&oper, const Vsol&vsol, const Param&param,
     Real (&topBLStates)[7],Real (&botBLStates)[7],const Real Uinf, const Geom&geom,const Real x_target) {
 
@@ -244,14 +226,20 @@ void interpolate_at_95_both_surfaces(const Real* xcoords, const Real* states, co
     
     /* State order: theta, delta*, tau_max, Ue, dpdx, tau_wall, delta 99% thickness*/
     
-    //--------------------------------------------------------------------------------------------------------------- //
-    //----------------------------bot surface BL states interp ------------------------------------------------------ //
+    // find index of node before sampling position (top and bottom)
+    int foundIndexBot = find_interp_position(xcoords,0,30,x_target);
+    int foundIndexTop = find_interp_position(xcoords,Ncoords-30, Ncoords-1,x_target);
+
+    // find the indexes to to the interpolation over 
+    int topIdx[4] = {0},botIdx[4] = {0}, topN, botN ;
+    get_nodes(foundIndexTop,foundIndexBot,x_target,topIdx,topN,botIdx,botN,vsol);
+
+    interp_BL_states(topIdx,botIdx,topN,botN,x_target,xcoords,states,topBLStates,botBLStates);
     
-    int foundIndexBot = find_interp_position(xcoords,0,20,x_target);
-    interpolate_on_surface(xcoords, states, foundIndexBot, x_target, botBLStates,vsol);
-    Real dpdxBot = interpolate_dpdx(xcoords,Cps,foundIndexBot,x_target,oper,Uinf);
+    Real dpdxBot = interpolate_dpdx(xcoords,Cps,botIdx,botN,x_target,oper,geom,Uinf);
+    Real dpdxTop = interpolate_dpdx(xcoords,Cps,topIdx,topN,x_target,oper,geom,Uinf);
     
-    
+    // ---------------------- bottom surface dimensionals -----------------------------------------
     Real ignore;
     Real UeCorrected = (get_uk(botBLStates[3],param,ignore)) * Uinf;
     botBLStates[3] = UeCorrected;
@@ -262,23 +250,19 @@ void interpolate_at_95_both_surfaces(const Real* xcoords, const Real* states, co
     botBLStates[4] = dpdxBot;
 
     // tau_wall = Cf*(rho * ue^2) / 2
-    Real cfBot = interpolate_cf(xcoords,states,vsol,foundIndexBot,x_target,param);
+    Real cfBot = interpolate_cf(xcoords,states,botIdx,botN,x_target,vsol,param);
     Real tauWallBot = (cfBot/2) * oper.rho * botBLStates[3] * botBLStates[3] ;
     botBLStates[5] = tauWallBot ;
-
+    
     // now get 99% thickness
-    Real deltaBot;
+    Real deltaBot = 0.0;
     Real frictionVel = std::sqrt(tauWallBot/oper.rho) ;
-    turbulent_BL_profile_XFOIL(botBLStates[0],botBLStates[3],frictionVel,((1.789e-5)/oper.rho),deltaBot);
+    if (tauWallBot!=0.0){
+        turbulent_BL_profile_XFOIL(botBLStates[0],botBLStates[3],frictionVel,((1.789e-5)/oper.rho),deltaBot);
+    }
     botBLStates[6] = deltaBot;
 
-    //--------------------------------------------------------------------------------------------------------------- //
-    //------------------------------- top surface BL interp --------------------------------------------------------- //
-    
-    int foundIndexTop = find_interp_position(xcoords,Ncoords-20, Ncoords-1,x_target);
-    interpolate_on_surface(xcoords, states,foundIndexTop, x_target, topBLStates,vsol);
-    Real dpdxTop = interpolate_dpdx(xcoords,Cps,foundIndexTop,x_target,oper,Uinf);
-    
+    // --------------------------------- top surface dimensionals ----------------------------------------
     UeCorrected = (get_uk(topBLStates[3],param,ignore)) * Uinf;
     topBLStates[3] = UeCorrected;
     
@@ -287,16 +271,19 @@ void interpolate_at_95_both_surfaces(const Real* xcoords, const Real* states, co
 
     topBLStates[2] = tauMaxTop;
     topBLStates[4] = dpdxTop;
-
+    
     // tau_wall = Cf*(rho * ue^2) / 2
-    Real cfTop = interpolate_cf(xcoords,states,vsol,foundIndexTop,x_target,param);
+    Real cfTop = interpolate_cf(xcoords,states,topIdx,topN,x_target,vsol,param);
     Real tauWallTop = (cfTop/2) * oper.rho * topBLStates[3] * topBLStates[3] ;
     topBLStates[5] = tauWallTop ;
 
     // now get 99% thickness
-    Real deltaTop;
+    Real deltaTop = 0.0;
     frictionVel = std::sqrt(tauWallTop/oper.rho) ;
-    turbulent_BL_profile_XFOIL(topBLStates[0],topBLStates[3],frictionVel,((1.789e-5)/oper.rho),deltaTop);
+    if (tauWallTop!=0.0){
+        turbulent_BL_profile_XFOIL(topBLStates[0],topBLStates[3],frictionVel,((1.789e-5)/oper.rho),deltaTop);
+    }
+    
     topBLStates[6] = deltaTop;
 
 }
