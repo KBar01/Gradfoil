eec2894758703a8476d36092f8a50b861d94c9d0
KBar01 <pa20830@bristol.ac.uk>
2025-11-04 19:04:38 +0000

    fixcm

--- src/get_cl.cpp
+++ src/get_cl.cpp
@@ -20,7 +20,6 @@ void calc_force(const Oper&op, const Geom&geom, const Param&par, const Isol&isol
     // Pre-allocate variables for memory efficiency
     Real cos_alpha = std::cos(op.alpha);
     Real sin_alpha = std::sin(op.alpha);
-    Real cp1, cp2, cpbar, dx, dy;
     
     // Loop over panels for integration
     for (int i0 = 1; i0 <= Ncoords; ++i0) {
@@ -35,8 +34,10 @@ void calc_force(const Oper&op, const Geom&geom, const Param&par, const Isol&isol
         Real dx1[2] = {x1[0] - geom.xref[0], x1[1] - geom.xref[1]};
         Real dx2[2] = {x2[0] - geom.xref[0], x2[1] - geom.xref[1]};
 
-        Real dx1nds = dxv[0] * dx1[0] + dxv[1] * dx1[1];
-        Real dx2nds = dxv[0] * dx2[0] + dxv[1] * dx2[1];
+        //Real dx1nds = dxv[0] * dx1[0] + dxv[1] * dx1[1];
+        //Real dx2nds = dxv[0] * dx2[0] + dxv[1] * dx2[1];
+        Real dx1nds = (x1[0] - geom.xref[0]) * dxv[1] + (x1[1] - geom.xref[1]) * dxv[0];
+        Real dx2nds = (x2[0] - geom.xref[0]) * dxv[1] - (x2[1] - geom.xref[1]) * dxv[0];
 
         Real dx = -dxv[0] * cos_alpha - dxv[1] * sin_alpha;
         Real dz =  dxv[1] * cos_alpha - dxv[0] * sin_alpha;
